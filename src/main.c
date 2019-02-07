24b093e757598be71a819d90112adb229a6eb78c
Seongbin Lim <seongbin.lim@rover.polytechnique.fr>
2019-02-07 16:24:27 +0100

    omp: sobel filter

--- src/main.c
+++ src/main.c
@@ -820,59 +820,63 @@ apply_sobel_filter( animated_gif * image )
 
         sobel = (pixel *)malloc(width * height * sizeof( pixel ) ) ;
 
-        for(j=1; j<height-1; j++)
+        #pragma omp parallel default(none) private(j,k) shared(i,width,height,p,sobel) //***
         {
-            for(k=1; k<width-1; k++)
+            #pragma omp for collapse(2) schedule(static)
+            for(j=1; j<height-1; j++)
             {
-                int pixel_blue_no, pixel_blue_n, pixel_blue_ne;
-                int pixel_blue_so, pixel_blue_s, pixel_blue_se;
-                int pixel_blue_o , pixel_blue  , pixel_blue_e ;
-
-                float deltaX_blue ;
-                float deltaY_blue ;
-                float val_blue;
-
-                pixel_blue_no = p[i][CONV(j-1,k-1,width)].b ;
-                pixel_blue_n  = p[i][CONV(j-1,k  ,width)].b ;
-                pixel_blue_ne = p[i][CONV(j-1,k+1,width)].b ;
-                pixel_blue_so = p[i][CONV(j+1,k-1,width)].b ;
-                pixel_blue_s  = p[i][CONV(j+1,k  ,width)].b ;
-                pixel_blue_se = p[i][CONV(j+1,k+1,width)].b ;
-                pixel_blue_o  = p[i][CONV(j  ,k-1,width)].b ;
-                pixel_blue    = p[i][CONV(j  ,k  ,width)].b ;
-                pixel_blue_e  = p[i][CONV(j  ,k+1,width)].b ;
-
-                deltaX_blue = -pixel_blue_no + pixel_blue_ne - 2*pixel_blue_o + 2*pixel_blue_e - pixel_blue_so + pixel_blue_se;
-
-                deltaY_blue = pixel_blue_se + 2*pixel_blue_s + pixel_blue_so - pixel_blue_ne - 2*pixel_blue_n - pixel_blue_no;
-
-                val_blue = sqrt(deltaX_blue * deltaX_blue + deltaY_blue * deltaY_blue)/4;
-
-
-                if ( val_blue > 50 )
+                for(k=1; k<width-1; k++)
                 {
-                    sobel[CONV(j  ,k  ,width)].r = 255 ;
-                    sobel[CONV(j  ,k  ,width)].g = 255 ;
-                    sobel[CONV(j  ,k  ,width)].b = 255 ;
-                } else
-                {
-                    sobel[CONV(j  ,k  ,width)].r = 0 ;
-                    sobel[CONV(j  ,k  ,width)].g = 0 ;
-                    sobel[CONV(j  ,k  ,width)].b = 0 ;
+                    int pixel_blue_no, pixel_blue_n, pixel_blue_ne;
+                    int pixel_blue_so, pixel_blue_s, pixel_blue_se;
+                    int pixel_blue_o , pixel_blue  , pixel_blue_e ;
+
+                    float deltaX_blue ;
+                    float deltaY_blue ;
+                    float val_blue;
+
+                    pixel_blue_no = p[i][CONV(j-1,k-1,width)].b ;
+                    pixel_blue_n  = p[i][CONV(j-1,k  ,width)].b ;
+                    pixel_blue_ne = p[i][CONV(j-1,k+1,width)].b ;
+                    pixel_blue_so = p[i][CONV(j+1,k-1,width)].b ;
+                    pixel_blue_s  = p[i][CONV(j+1,k  ,width)].b ;
+                    pixel_blue_se = p[i][CONV(j+1,k+1,width)].b ;
+                    pixel_blue_o  = p[i][CONV(j  ,k-1,width)].b ;
+                    pixel_blue    = p[i][CONV(j  ,k  ,width)].b ;
+                    pixel_blue_e  = p[i][CONV(j  ,k+1,width)].b ;
+
+                    deltaX_blue = -pixel_blue_no + pixel_blue_ne - 2*pixel_blue_o + 2*pixel_blue_e - pixel_blue_so + pixel_blue_se;
+
+                    deltaY_blue = pixel_blue_se + 2*pixel_blue_s + pixel_blue_so - pixel_blue_ne - 2*pixel_blue_n - pixel_blue_no;
+
+                    val_blue = sqrt(deltaX_blue * deltaX_blue + deltaY_blue * deltaY_blue)/4;
+
+
+                    if ( val_blue > 50 )
+                    {
+                        sobel[CONV(j  ,k  ,width)].r = 255 ;
+                        sobel[CONV(j  ,k  ,width)].g = 255 ;
+                        sobel[CONV(j  ,k  ,width)].b = 255 ;
+                    } else
+                    {
+                        sobel[CONV(j  ,k  ,width)].r = 0 ;
+                        sobel[CONV(j  ,k  ,width)].g = 0 ;
+                        sobel[CONV(j  ,k  ,width)].b = 0 ;
+                    }
                 }
             }
-        }
-
-        for(j=1; j<height-1; j++)
-        {
-            for(k=1; k<width-1; k++)
+        
+            #pragma omp for collapse(2) schedule(static)
+            for(j=1; j<height-1; j++)
             {
-                p[i][CONV(j  ,k  ,width)].r = sobel[CONV(j  ,k  ,width)].r ;
-                p[i][CONV(j  ,k  ,width)].g = sobel[CONV(j  ,k  ,width)].g ;
-                p[i][CONV(j  ,k  ,width)].b = sobel[CONV(j  ,k  ,width)].b ;
+                for(k=1; k<width-1; k++)
+                {
+                    p[i][CONV(j  ,k  ,width)].r = sobel[CONV(j  ,k  ,width)].r ;
+                    p[i][CONV(j  ,k  ,width)].g = sobel[CONV(j  ,k  ,width)].g ;
+                    p[i][CONV(j  ,k  ,width)].b = sobel[CONV(j  ,k  ,width)].b ;
+                }
             }
-        }
-
+        } // #pragma omp parallel ends
         free (sobel) ;
     }
 
