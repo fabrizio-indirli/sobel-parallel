20e180b022b44155b2eee8d0a13abb7ddc0ee07c
Seongbin Lim <seongbin.lim@irlande.polytechnique.fr>
2019-02-12 11:09:35 +0100

    CHECKPOINT: blur filter done, so far...

--- src/main.c
+++ src/main.c
@@ -697,7 +697,7 @@ apply_blur_filter( animated_gif * image, int size, int threshold ) // 5, 20
                 //*** one thing that if checks only j not k...
                 //*** WHAT IF not using `collapse`?
                 // #pragma omp for collapse(2) schedule(static,width) 
-                #pragma omp for collapse(2) schedule(dynamic,width) 
+                #pragma omp for collapse(2) schedule(dynamic, width) 
                 for(j=size; j<height-size;j++)
                 {
                     for(k=size; k<width-size; k++)
@@ -752,7 +752,7 @@ apply_blur_filter( animated_gif * image, int size, int threshold ) // 5, 20
                             p[i][CONV(j  ,k  ,width)].b = new[CONV(j  ,k  ,width)].b ;
 
                         } 
-                        // middle part
+                        // Middle part
                         else 
                         {
                             new[CONV(j,k,width)].r = p[i][CONV(j,k,width)].r ;
