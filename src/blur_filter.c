7fef946914a893706e25680b1d4ce93b8d61794d
milo-sb <milo-sb@github.com>
2019-03-10 12:14:08 +0100

    *** Applied omp to all filters

--- src/blur_filter.c
+++ src/blur_filter.c
@@ -22,98 +22,112 @@ apply_blur_filter( int width, int height, pixel * pi, int size, int threshold ) 
     {
         end = 1 ;
         n_iter++ ;
-
-        /* Apply blur on top part of image (10%) */
-        for(j=size; j<height/10-size; j++)
+        #pragma omp parallel default(none) private(j,k) shared(size,threshold,width,height,pi,new,end)
         {
-            for(k=size; k<width-size; k++)
-            {
-                int stencil_j, stencil_k ;
-                int t_r = 0 ;
-                int t_g = 0 ;
-                int t_b = 0 ;
+            int N_j, N_k;
+            int S_j;
 
-                for ( stencil_j = -size ; stencil_j <= size ; stencil_j++ )
+            /* Apply blur on top part of image (10%) */
+            N_j = height/10-size;
+            N_k = width-size;
+            #pragma omp for collapse(2) schedule(static,width) 
+            for(j=size; j<N_j; j++)
+            {
+                for(k=size; k<N_k; k++)
                 {
-                    for ( stencil_k = -size ; stencil_k <= size ; stencil_k++ )
+                    int stencil_j, stencil_k ;
+                    int t_r = 0 ;
+                    int t_g = 0 ;
+                    int t_b = 0 ;
+
+                    for ( stencil_j = -size ; stencil_j <= size ; stencil_j++ )
                     {
-                        t_r += pi[CONV(j+stencil_j,k+stencil_k,width)].r ;
-                        t_g += pi[CONV(j+stencil_j,k+stencil_k,width)].g ;
-                        t_b += pi[CONV(j+stencil_j,k+stencil_k,width)].b ;
+                        for ( stencil_k = -size ; stencil_k <= size ; stencil_k++ )
+                        {
+                            t_r += pi[CONV(j+stencil_j,k+stencil_k,width)].r ;
+                            t_g += pi[CONV(j+stencil_j,k+stencil_k,width)].g ;
+                            t_b += pi[CONV(j+stencil_j,k+stencil_k,width)].b ;
+                        }
                     }
+
+                    new[CONV(j,k,width)].r = t_r / ( (2*size+1)*(2*size+1) ) ;
+                    new[CONV(j,k,width)].g = t_g / ( (2*size+1)*(2*size+1) ) ;
+                    new[CONV(j,k,width)].b = t_b / ( (2*size+1)*(2*size+1) ) ;
                 }
-
-                new[CONV(j,k,width)].r = t_r / ( (2*size+1)*(2*size+1) ) ;
-                new[CONV(j,k,width)].g = t_g / ( (2*size+1)*(2*size+1) ) ;
-                new[CONV(j,k,width)].b = t_b / ( (2*size+1)*(2*size+1) ) ;
             }
-        }
 
-        /* Copy the middle part of the image */
-        for(j=height/10-size; j<height*0.9+size; j++)
-        {
-            for(k=size; k<width-size; k++)
+            /* Copy the middle part of the image */
+            S_j = height/10-size;
+            N_j = height*0.9+size;
+            #pragma omp for collapse(2) schedule(static,width) 
+            for(j=S_j; j<N_j; j++)
             {
-                new[CONV(j,k,width)].r = pi[CONV(j,k,width)].r ; 
-                new[CONV(j,k,width)].g = pi[CONV(j,k,width)].g ; 
-                new[CONV(j,k,width)].b = pi[CONV(j,k,width)].b ; 
-            }
-        }
-
-        /* Apply blur on the bottom part of the image (10%) */
-        for(j=height*0.9+size; j<height-size; j++)
-        {
-            for(k=size; k<width-size; k++)
-            {
-                int stencil_j, stencil_k ;
-                int t_r = 0 ;
-                int t_g = 0 ;
-                int t_b = 0 ;
-
-                for ( stencil_j = -size ; stencil_j <= size ; stencil_j++ )
+                for(k=size; k<N_k; k++)
                 {
-                    for ( stencil_k = -size ; stencil_k <= size ; stencil_k++ )
-                    {
-                        t_r += pi[CONV(j+stencil_j,k+stencil_k,width)].r ;
-                        t_g += pi[CONV(j+stencil_j,k+stencil_k,width)].g ;
-                        t_b += pi[CONV(j+stencil_j,k+stencil_k,width)].b ;
-                    }
+                    new[CONV(j,k,width)].r = pi[CONV(j,k,width)].r ; 
+                    new[CONV(j,k,width)].g = pi[CONV(j,k,width)].g ; 
+                    new[CONV(j,k,width)].b = pi[CONV(j,k,width)].b ; 
                 }
-
-                new[CONV(j,k,width)].r = t_r / ( (2*size+1)*(2*size+1) ) ;
-                new[CONV(j,k,width)].g = t_g / ( (2*size+1)*(2*size+1) ) ;
-                new[CONV(j,k,width)].b = t_b / ( (2*size+1)*(2*size+1) ) ;
             }
-        }
 
-        for(j=1; j<height-1; j++)
-        {
-            for(k=1; k<width-1; k++)
+            /* Apply blur on the bottom part of the image (10%) */
+            S_j = height*0.9+size;
+            N_j = height-size;
+            #pragma omp for collapse(2) schedule(static,width) 
+            for(j=S_j; j<N_j; j++)
             {
+                for(k=size; k<N_k; k++)
+                {
+                    int stencil_j, stencil_k ;
+                    int t_r = 0 ;
+                    int t_g = 0 ;
+                    int t_b = 0 ;
 
-                float diff_r ;
-                float diff_g ;
-                float diff_b ;
+                    for ( stencil_j = -size ; stencil_j <= size ; stencil_j++ )
+                    {
+                        for ( stencil_k = -size ; stencil_k <= size ; stencil_k++ )
+                        {
+                            t_r += pi[CONV(j+stencil_j,k+stencil_k,width)].r ;
+                            t_g += pi[CONV(j+stencil_j,k+stencil_k,width)].g ;
+                            t_b += pi[CONV(j+stencil_j,k+stencil_k,width)].b ;
+                        }
+                    }
 
-                diff_r = (new[CONV(j  ,k  ,width)].r - pi[CONV(j  ,k  ,width)].r) ;
-                diff_g = (new[CONV(j  ,k  ,width)].g - pi[CONV(j  ,k  ,width)].g) ;
-                diff_b = (new[CONV(j  ,k  ,width)].b - pi[CONV(j  ,k  ,width)].b) ;
+                    new[CONV(j,k,width)].r = t_r / ( (2*size+1)*(2*size+1) ) ;
+                    new[CONV(j,k,width)].g = t_g / ( (2*size+1)*(2*size+1) ) ;
+                    new[CONV(j,k,width)].b = t_b / ( (2*size+1)*(2*size+1) ) ;
+                }
+            }
 
-                if ( diff_r > threshold || -diff_r > threshold 
-                        ||
-                            diff_g > threshold || -diff_g > threshold
+            #pragma omp for collapse(2) schedule(static,width) 
+            for(j=1; j<height-1; j++)
+            {
+                for(k=1; k<width-1; k++)
+                {
+
+                    float diff_r ;
+                    float diff_g ;
+                    float diff_b ;
+
+                    diff_r = (new[CONV(j  ,k  ,width)].r - pi[CONV(j  ,k  ,width)].r) ;
+                    diff_g = (new[CONV(j  ,k  ,width)].g - pi[CONV(j  ,k  ,width)].g) ;
+                    diff_b = (new[CONV(j  ,k  ,width)].b - pi[CONV(j  ,k  ,width)].b) ;
+
+                    if ( diff_r > threshold || -diff_r > threshold 
                             ||
-                            diff_b > threshold || -diff_b > threshold
-                    ) {
-                    end = 0 ;
+                                diff_g > threshold || -diff_g > threshold
+                                ||
+                                diff_b > threshold || -diff_b > threshold
+                        ) {
+                        end = 0 ;
+                    }
+
+                    pi[CONV(j  ,k  ,width)].r = new[CONV(j  ,k  ,width)].r ;
+                    pi[CONV(j  ,k  ,width)].g = new[CONV(j  ,k  ,width)].g ;
+                    pi[CONV(j  ,k  ,width)].b = new[CONV(j  ,k  ,width)].b ;
                 }
-
-                pi[CONV(j  ,k  ,width)].r = new[CONV(j  ,k  ,width)].r ;
-                pi[CONV(j  ,k  ,width)].g = new[CONV(j  ,k  ,width)].g ;
-                pi[CONV(j  ,k  ,width)].b = new[CONV(j  ,k  ,width)].b ;
             }
-        }
-
+        } // omp parallel
     } while ( threshold > 0 && !end ) ;
 
     // printf( "Nb iter for image %d\n", n_iter ) ;int i=0; i<N; i++)
--- src/sobel_filter.c
+++ src/sobel_filter.c
@@ -1,4 +1,5 @@
 #include "sobel_filter.h"
+#include <omp.h>
 
 #define CONV(l,c,nb_c) \
     (l)*(nb_c)+(c)
@@ -10,58 +11,63 @@ void apply_sobel_filter(int width, int height, pixel * pi){
     pixel * sobel ;
     sobel = (pixel *)malloc(width * height * sizeof( pixel ) ) ;
 
-    // `dynamic` can be a better choice, since there is an if statement that might invoke imbalance for the iteration.
-    // Actually nope... static one is faster. 
-    for(j=1; j<height-1; j++)
+    #pragma omp parallel default(none) private(j,k) shared(width,height,pi,sobel)
     {
-        for(k=1; k<width-1; k++)
+        // `dynamic` can be a better choice, since there is an if statement that might invoke imbalance for the iteration.
+        // Actually nope... static one is faster. 
+        #pragma omp for collapse(2) schedule(static) 
+        for(j=1; j<height-1; j++)
         {
-            int pixel_blue_no, pixel_blue_n, pixel_blue_ne;
-            int pixel_blue_so, pixel_blue_s, pixel_blue_se;
-            int pixel_blue_o , pixel_blue  , pixel_blue_e ;
-
-            float deltaX_blue ;
-            float deltaY_blue ;
-            float val_blue;
-
-            pixel_blue_no = pi[CONV(j-1,k-1,width)].b ;
-            pixel_blue_n  = pi[CONV(j-1,k  ,width)].b ;
-            pixel_blue_ne = pi[CONV(j-1,k+1,width)].b ;
-            pixel_blue_so = pi[CONV(j+1,k-1,width)].b ;
-            pixel_blue_s  = pi[CONV(j+1,k  ,width)].b ;
-            pixel_blue_se = pi[CONV(j+1,k+1,width)].b ;
-            pixel_blue_o  = pi[CONV(j  ,k-1,width)].b ;
-            pixel_blue    = pi[CONV(j  ,k  ,width)].b ;
-            pixel_blue_e  = pi[CONV(j  ,k+1,width)].b ;
-
-            deltaX_blue = -pixel_blue_no + pixel_blue_ne - 2*pixel_blue_o + 2*pixel_blue_e - pixel_blue_so + pixel_blue_se;
-
-            deltaY_blue = pixel_blue_se + 2*pixel_blue_s + pixel_blue_so - pixel_blue_ne - 2*pixel_blue_n - pixel_blue_no;
-
-            val_blue = sqrt(deltaX_blue * deltaX_blue + deltaY_blue * deltaY_blue)/4;
-
-
-            if ( val_blue > 50 )
+            for(k=1; k<width-1; k++)
             {
-                sobel[CONV(j  ,k  ,width)].r = 255 ;
-                sobel[CONV(j  ,k  ,width)].g = 255 ;
-                sobel[CONV(j  ,k  ,width)].b = 255 ;
-            } else
-            {
-                sobel[CONV(j  ,k  ,width)].r = 0 ;
-                sobel[CONV(j  ,k  ,width)].g = 0 ;
-                sobel[CONV(j  ,k  ,width)].b = 0 ;
+                int pixel_blue_no, pixel_blue_n, pixel_blue_ne;
+                int pixel_blue_so, pixel_blue_s, pixel_blue_se;
+                int pixel_blue_o , pixel_blue  , pixel_blue_e ;
+
+                float deltaX_blue ;
+                float deltaY_blue ;
+                float val_blue;
+
+                pixel_blue_no = pi[CONV(j-1,k-1,width)].b ;
+                pixel_blue_n  = pi[CONV(j-1,k  ,width)].b ;
+                pixel_blue_ne = pi[CONV(j-1,k+1,width)].b ;
+                pixel_blue_so = pi[CONV(j+1,k-1,width)].b ;
+                pixel_blue_s  = pi[CONV(j+1,k  ,width)].b ;
+                pixel_blue_se = pi[CONV(j+1,k+1,width)].b ;
+                pixel_blue_o  = pi[CONV(j  ,k-1,width)].b ;
+                pixel_blue    = pi[CONV(j  ,k  ,width)].b ;
+                pixel_blue_e  = pi[CONV(j  ,k+1,width)].b ;
+
+                deltaX_blue = -pixel_blue_no + pixel_blue_ne - 2*pixel_blue_o + 2*pixel_blue_e - pixel_blue_so + pixel_blue_se;
+
+                deltaY_blue = pixel_blue_se + 2*pixel_blue_s + pixel_blue_so - pixel_blue_ne - 2*pixel_blue_n - pixel_blue_no;
+
+                val_blue = sqrt(deltaX_blue * deltaX_blue + deltaY_blue * deltaY_blue)/4;
+
+
+                if ( val_blue > 50 )
+                {
+                    sobel[CONV(j  ,k  ,width)].r = 255 ;
+                    sobel[CONV(j  ,k  ,width)].g = 255 ;
+                    sobel[CONV(j  ,k  ,width)].b = 255 ;
+                } else
+                {
+                    sobel[CONV(j  ,k  ,width)].r = 0 ;
+                    sobel[CONV(j  ,k  ,width)].g = 0 ;
+                    sobel[CONV(j  ,k  ,width)].b = 0 ;
+                }
             }
         }
-    }
 
-    for(j=1; j<height-1; j++)
-    {
-        for(k=1; k<width-1; k++)
+        #pragma omp for collapse(2) schedule(static)
+        for(j=1; j<height-1; j++)
         {
-            pi[CONV(j  ,k  ,width)].r = sobel[CONV(j  ,k  ,width)].r ;
-            pi[CONV(j  ,k  ,width)].g = sobel[CONV(j  ,k  ,width)].g ;
-            pi[CONV(j  ,k  ,width)].b = sobel[CONV(j  ,k  ,width)].b ;
+            for(k=1; k<width-1; k++)
+            {
+                pi[CONV(j  ,k  ,width)].r = sobel[CONV(j  ,k  ,width)].r ;
+                pi[CONV(j  ,k  ,width)].g = sobel[CONV(j  ,k  ,width)].g ;
+                pi[CONV(j  ,k  ,width)].b = sobel[CONV(j  ,k  ,width)].b ;
+            }
         }
     }
     free(sobel) ;
