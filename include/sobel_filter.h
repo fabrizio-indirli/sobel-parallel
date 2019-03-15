c5c0385875fed990abe0568bb780572bbbfa9aac
fabrizio-indirli <fabrizio.indirli@mail.polimi.it>
2019-03-15 16:12:47 +0100

    testing with cuda

--- include/sobel_filter.h
+++ include/sobel_filter.h
@@ -2,4 +2,11 @@
 #include <stdlib.h>
 #include <math.h>
 
-void apply_sobel_filter(int width, int height, pixel * pi);
+#ifdef __cplusplus
+extern "C"{
+#endif 
+    void apply_sobel_filter(int width, int height, pixel * pi);
+#ifdef __cplusplus
+}
+#endif
+
