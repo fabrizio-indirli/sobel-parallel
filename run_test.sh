618df619bf3985b44c68fab5e339091330b645ce
seongbin lim <sungbin246@gmail.com>
2019-03-17 19:17:06 +0100

    1st and 2nd benchmark

--- run_test.sh
+++ run_test.sh
@@ -6,10 +6,12 @@ INPUT_DIR=images/original
 OUTPUT_DIR=images/processed
 mkdir $OUTPUT_DIR 2>/dev/null
 
-
+for j in {1..20}; do
 for i in $INPUT_DIR/*gif ; do
     DEST=$OUTPUT_DIR/`basename $i .gif`-sobel.gif
     echo "Running test on $i -> $DEST"
 
+    # OMP_NUM_THREADS=8 salloc –n 1 –N 1 mpirun . # Hybrid
     ./sobelf $i $DEST
+done
 done
